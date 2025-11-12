a94bce634890365a95738ec9d777d259fe22b5f6
omar cetzal <omarc.developer@hotmail.com>
2025-11-11 21:36:39 -0600

    feat-user: implemetado la capa se servicios es el encardado de la logica o negocio. -implementando la funcion para obtener usarios

---
+++ backend/api/users/services.py
@@ -0,0 +1,16 @@
+from .repository import UserRepository
+
+class UserService:
+    """
+    La clase UserService encapsula la lógica empresarial para las operaciones del usuario. 
+    Actúa como intermediario entre la capa de presentación (vistas) y la capa de acceso a datos (repositorio), 
+    que organiza los pasos necesarios para cumplir un caso de uso.
+    """
+
+    def __init__(self):
+        self.repository = UserRepository()
+
+    def get_all_users(self, filters=None):
+        """Obtener todo los usarios"""
+        return self.repository.get_all_users(filters)
+
