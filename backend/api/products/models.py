81bb276c53686d04c8f8042312bcc977463de231
omar cetzal <omarc.developer@hotmail.com>
2025-11-12 00:13:26 -0600

    feat-products:se implementó el modelo de productos junto con su migración
    - Se creó el modelo `Product` en la app `products`.
    - Campos: name,price, stock, activo, created_at, last_update.
    -se definio los tipos de datos cada campo.
    - Se generó y aplicó la migración inicial.
    - Preparación para futura integración con serializers y vistas DRF.

--- backend/api/products/models.py
+++ backend/api/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Modelo que representa un producto en el sistema
+    """
+    nombre = models.CharField(max_length=255, verbose_name="Nombre del producto")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio del producto")
+    stock = models.PositiveIntegerField(verbose_name="Cantidad en stock")
+    activo = models.BooleanField(default=True, verbose_name="Producto activo")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True, verbose_name="Última Actualización")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        ordering = ['id']
