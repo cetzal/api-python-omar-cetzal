97eb4297d09b77d2d75593a2977040f2cbc6d827
omar cetzal <omarc.developer@hotmail.com>
2025-11-11 20:54:05 -0600

    feat-user: configuro la clase modelo con los campos requerido y tambien se extendio para usar la tabala base de djando

--- backend/api/users/models.py
+++ backend/api/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    se usara la tabla sabe que proporciona djando y solo se agrega los campos faltantes
+    """
+    age = models.PositiveIntegerField(verbose_name="Edad", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True, verbose_name="Última Actualización")
+
+    # se puede agrega mas campos si se requiere
+
+    def __str__(self):
+        return self.username
