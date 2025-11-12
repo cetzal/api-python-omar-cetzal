079607771267071cf801306c139c31db059fcbbc
omar cetzal <omarc.developer@hotmail.com>
2025-11-11 23:37:11 -0600

    habilitando modulo de productos

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -33,4 +33,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # API endpoints
+    path('api/users/', include('api.users.urls')),
+    
 ]
