4fe65563dd6327a795d6f0980d427a626b0553a9
omar cetzal <omarc.developer@hotmail.com>
2025-11-11 19:33:42 -0600

    feat-dev : configurar Swagger UI para documentación automática
    - Instalar e configurar drf-yasg en settings.py
    - Agregar rutas para Swagger UI
    - Configurar schema view con información básica de la API

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
